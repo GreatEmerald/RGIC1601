bcc0368da40d1dfc6b35e51a1dca022cb259c977
GreatEmerald <pastas4@gmail.com>
2016-05-28 01:04:03 +0200

    Addition of a basic file template for the backend.
    Also a small reorganisation of directories.

---
+++ backend/src/main.r
@@ -0,0 +1,26 @@
+# Main script for the backend of the management zone generation tool
+# Copyright (C) 2016 Dainius Masiliunas
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## Note: This script only brings different modules in modules/ together.
+## No code belonging to one of the modules should go here.
+
+#### Import modules ####
+
+source(modules/input.r)
+
+#### Main script ####
+
+InputImage = Input()
---
+++ backend/src/modules/input.r
@@ -0,0 +1,44 @@
+# Input handling module for the backend of the management zone generation tool
+# Copyright (C) 2016 Dainius Masiliunas
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#### Required libraries ####
+
+library(rgdal)
+
+
+## Function for importing spatial files into the backend, using GDAL
+#
+# Arguments:
+#   filename:
+#       character string: Raster file to use as input. Must be a format handled by GDAL.
+#       character vector: Raster files to use as input, treated as bands of one file.
+#           Must be a format handled by GDAL.
+#
+# Maintains:
+#   Environment
+#   Input files
+#
+# On violation:
+#   Prints an error() and returns NA
+#
+# Returns:
+#   A (multiband) SpatialGridDataFrame object containing the input raster,
+#   stacked into one if there were multiple input files.
+
+Input = function(filename = "../data/input.tif")
+{
+
+}
