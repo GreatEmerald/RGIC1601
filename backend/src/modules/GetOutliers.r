243a76a75c15305e99548e13ef2b9aaa17a73657
wschuc002 <william.schuch@wur.nl>
2016-06-12 15:06:50 +0200

    Works with Cumulative, but not with Homogeneous ZMZ raster. Clean up.

--- backend/src/modules/GetOutliers.r
+++ backend/src/modules/GetOutliers.r
@@ -21,6 +21,7 @@ library(sp)
 library(spatstat)
 library(maptools)
 library(rgeos)
+library(igraph)
 
 ## Function for detecting outliers in a single band rater file, using GDAL
 #
@@ -43,16 +44,6 @@ library(rgeos)
 #   A Formal Class SpatialPointsDataFrame,
 #   with the outliers.
 
-#rm(list = ls())  # Clear the workspace!
-#ls() ## no objects left in the workspace
-#start.time = Sys.time()
-
-
-getwd()
-WS = raster(file.path("..", ".." , "data", "2016-04-03_bert_boerma_kale_grond_index_cumulative_TestArea.tif"))
-WS = raster(file.path("..", ".." , "data", "2016-04-03_bert_boerma_kale_grond_index_cumulative.tif"))
-#WSS = stack(WS)
-#WSB = brick(WS)
 
 # INTO THE FUNCTION
 GetOutliers = function(rast_in, Q)
@@ -61,171 +52,43 @@ GetOutliers = function(rast_in, Q)
   {
     stop(paste("Input", (data.class(rast_in)), "is not single-banded."))
   }
+  UV = unique(rast_in)
   
-
-  quantiles = quantile(rast_in, c(Q,1-Q))
-  Vec = rasterToPoints(rast_in, fun=function(x){x>quantiles[[2]] | x<quantiles[[1]]}, spatial=TRUE)
-  #spplot(rast_in, sp.layout=list(Vec))
+  if (length(UV) > 20) # used for 'Singe-band image'
+  {
+    quantiles = quantile(rast_in, c(Q,1-Q))
+    Vec = rasterToPoints(rast_in, fun=function(x){x>quantiles[[2]] | x<quantiles[[1]]}, spatial=TRUE)
+    #spplot(rast_in, sp.layout=list(Vec))
   
-  sSp = as(Vec, "ppp")  # convert points to pp class
-  Dens = density(sSp, adjust = 0.1)  # create density object
-  Dsg = as(Dens, "SpatialGridDataFrame")  # convert to spatial grid class
-  Dim = as.image.SpatialGridDataFrame(Dsg)  # convert again to an image
-  Dcl = contourLines(Dim, nlevels = 8)  # create contour object - change 8 for more/fewer levels
-  SLDF = ContourLines2SLDF(Dcl)  # convert to SpatialLinesDataFrame
-  Polyclust = gPolygonize(SLDF[5, ])
-  gas = gArea(Polyclust, byid = T)/10000
-  Polyclust = SpatialPolygonsDataFrame(Polyclust, data = data.frame(gas), match.ID = F)
-  centroids = getSpPPolygonsLabptSlots(Polyclust)
-  centroidsDF = as.data.frame(centroids)
-  centroidsSPDF = SpatialPointsDataFrame(centroidsDF, data = data.frame(gas))
+    sSp = as(Vec, "ppp")  # convert points to pp class
+    Dens = density(sSp, adjust = 0.1)  # create density object
+    Dsg = as(Dens, "SpatialGridDataFrame")  # convert to spatial grid class
+    Dim = as.image.SpatialGridDataFrame(Dsg)  # convert again to an image
+    Dcl = contourLines(Dim, nlevels = 8)  # create contour object - change 8 for more/fewer levels
+    SLDF = ContourLines2SLDF(Dcl)  # convert to SpatialLinesDataFrame
+    Polyclust = gPolygonize(SLDF[5, ])
+    gas = gArea(Polyclust, byid = T)/10000
+    Polyclust = SpatialPolygonsDataFrame(Polyclust, data = data.frame(gas), match.ID = F)
+    centroids = getSpPPolygonsLabptSlots(Polyclust)
+    centroidsDF = as.data.frame(centroids)
+    centroidsSPDF = SpatialPointsDataFrame(centroidsDF, data = data.frame(gas))
   
-  return(list(rast_in, quantiles, Vec, Polyclust, centroidsSPDF))
+    return(list(rast_in, quantiles, Vec, Polyclust, centroidsSPDF, Dsg))
+  }
+  if (length(UV) <= 20) # used for 'Homogeneous MZ raster'
+  {
+    Vec_MZ2 = RasterToVector(rast_in[[2]])
+    gas = gArea(Vec_MZ2, byid = T)/10000
+    Polyclust = SpatialPolygonsDataFrame(Vec_MZ2, data = data.frame(gas), match.ID = F)
+    
+    #centroids = getSpPPolygonsLabptSlots(Polyclust)
+    #centroidsDF = as.data.frame(centroids)
+    #centroidsSPDF = SpatialPointsDataFrame(centroidsDF, data = data.frame(gas))
+    
+    #Dcl = contourLines(rast_in, nlevels = 1)  # create contour object - change 8 for more/fewer levels
+    
+    return(list(rast_in, Vec_MZ2, gas, Polyclust, centroidsSPDF))
+  }
 }
-GetOutl = GetOutliers(WS,0.005)
-spplot(GetOutl[[3]])
-
-rv = list("sp.polygons", GetOutl[[4]], fill = "red")
-
-SPplotWS = spplot(GetOutl[[1]], scales = list(draw = TRUE),
-       xlab = "X", ylab = "Y",
-       ol.regions = rainbow(99, start=.1),
-       sp.layout = c('sp.points', GetOutl[[3]], col='red', pch=16))
-       #sp.layout = rv)
-
-SPplotWS
-
-sSp = as(GetOutl[[3]], "ppp")  # convert points to pp class
-Dens = density(sSp, adjust = 0.1)  # create density object
-class(Dens)  # just for interest: it's got it's of pixel image class
-plot(Dens)  # default plot for density
-
-Dsg = as(Dens, "SpatialGridDataFrame")  # convert to spatial grid class
-SPplotDens = spplot(Dsg, scales = list(draw = TRUE),
-                  xlab = "X", ylab = "Y",
-                  ol.regions = rainbow(99, start=.1) ,
-                  sp.layout = c('sp.points', GetOutl[[3]], col='red', pch=16))
-SPplotDens
-
-contour(density(sSp, adjust = 0.2), nlevels = 4)  # plot as contours - this is where we're heading
-
-Dim <- as.image.SpatialGridDataFrame(Dsg)  # convert again to an image
-Dcl <- contourLines(Dim, nlevels = 8)  # create contour object - change 8 for more/fewer levels
-SLDF <- ContourLines2SLDF(Dcl)  # convert to SpatialLinesDataFrame
-plot(SLDF, col = terrain.colors(8))
-SPplotCont = spplot(SLDF, scales = list(draw = TRUE),
-                    xlab = "X", ylab = "Y",
-                    ol.regions = rainbow(99, start=.1))
-SPplotCont 
-                    
-
-Polyclust <- gPolygonize(SLDF[5, ])
-gas <- gArea(Polyclust, byid = T)/10000
-Polyclust <- SpatialPolygonsDataFrame(Polyclust, data = data.frame(gas), match.ID = F)
-plot(Polyclust)
-
-centroids <- getSpPPolygonsLabptSlots(Polyclust)
-plot(centroids)
-
-centroidsDF = as.data.frame(centroids)
-centroidsSPDF <- SpatialPointsDataFrame(centroidsDF, data = data.frame(gas))
-spplot(centroidsSPDF)
-
-cAg <- aggregate(GetOutl[[3]], by = Polyclust, FUN = length)
-# lb <- gBoundary(lnd)
-plot(Dens, main = "")
-plot(lnd, border = "grey", lwd = 2, add = T)
-plot(SLDF, col = terrain.colors(8), add = T)
-plot(cAg, col = "red", border = "white", add = T)
-graphics::text(coordinates(cAg) + 1000, labels = cAg$CODE)
-
-
-
-aggdata = aggregate(GetOutl[[3]], by=list(coord), FUN=mean, na.rm=TRUE)
-aggdata = aggregate(GetOutl[[3]], by=list(coord), FUN=mean)
-plot(aggdata)
-  
-
-### PREVIOUS CULCULATIONS (no functions) ###
-
-# Check if input (RasterLayer/RasterStack/RasterBrick) is singe-banded:
-  if (nbands(WS) > 1)
-  {
-    paste("Input", (data.class(WS)), "is not single-banded.")
-    #return(NA)
-  }
-
-  if (length(WSS@layers) > 1)
-  {
-    paste("Input", (data.class(WSS)), "is not single-banded.")
-    #return(NA)
-  }
-  if (nbands(WSB) > 1)
-  {
-    paste("Input", (data.class(WSB)), "is not single-banded.")
-    #return(NA)
-  }
-
-#plot a histogram
-hist(WS, maxpixels=1000000, plot=TRUE, freq=TRUE, breaks=200)
-
-#calculate values to detect outliers
-WSmean = cellStats(WS,mean)
-WSmin = cellStats(WS,min)
-WSmax = cellStats(WS,max)
-quantiles = quantile(WS, c(.001, .999))
-
-# Extract the max and the min outliers
-WS_outliers_min = WS < quantiles[[1]]
-WS_outliers_max = WS > quantiles[[2]]
-
-# Create spatial point dataframe
-Vec = rasterToPoints(WS,fun=function(x){x>quantiles[[2]] | x<quantiles[[1]]}, spatial=TRUE)
-
-#plotting soem results
-plot(WS_outliers_min)
-plot(WS_outliers_max)
-plot(Vec)
-plot(WS)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# reproject
-prj_string_RD <- CRS("+proj=sterea +lat_0=52.15616055555555
-										 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000
-                     +ellps=bessel +towgs84=565.2369,50.0087,465.658,
-                     -0.406857330322398,0.350732676542563,-1.8703473836068,
-                     4.0812 +units=m +no_defs")
-
-prj_string_WGS84_31 <- CRS("+proj=utm +zone=31 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
-
-PROJ4WS = WS@crs
-
-Vec@proj4string = prj_string_RD
-Vec@proj4string = prj_string_WGS84_31
-Vec@proj4string = WS@crs
-
-ptransform(Vec, '+proj=latlong +ellps=sphere',
-           '+proj=merc +ellps=sphere')
-
-
-#Write as geojson
-writeOGR(Vec, 'test5','Vec', driver='GeoJSON')
-file.rename("test5", "test5.geojson")
-
-## _ _ _ _
-## finishing the script and calculate running time of the script
-#end.time = Sys.time()
-#time.taken = end.time - start.time
-#paste("The script has finished running in", time.taken, "seconds.")
+#GetOutl = GetOutliers(WS,0.005) # used for 'Singe-band image'
+#GetOutl_MZ = GetOutliers(HomogeniseRaster[[2]],0.005) # used for 'Homogeneous MZ raster'
