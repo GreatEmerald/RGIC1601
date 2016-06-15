c50077e60cf3b78bf53f39de675feac401926332
merijn <merijnslagter@gmail.com>
2016-06-15 17:30:02 +0200

    Only copy of GetComponent commented out

---
+++ backend/src/modules/CalculateIndex.r
@@ -0,0 +1,82 @@
+# Input handling module for the backend of the management zone generation tool
+# Copyright (C) 2016 Merijn Slagter
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#### Required libraries ####
+
+library(raster)
+
+#### Calculates indices based on "soil" or "vegetation" ####
+
+# Arguments:
+#   Object:
+#       Raster object; multi layered brick/stack
+#	Mask: raster object
+#   Additional argument: (recommended for saving memory space)
+#       Aggregation Factor
+#
+# Maintains:
+#   Environment
+#   Input files
+#
+# On violation:
+#   warning()
+#
+# Returns:
+#   Raster single-band layer object; the 
+#   layer will be the first principle component of the input brick
+
+#setwd(".." ,"..", "/RGIC01/backend/data/")
+#file1 = raster("2016-04-03_bert_boerma_kale_grond_transparent_reflectance_green.tif")
+#file2 = raster("2016-04-03_bert_boerma_kale_grond_transparent_reflectance_red.tif")
+#file3 = raster("2016-04-03_bert_boerma_kale_grond_transparent_reflectance_red edge.tif")
+#file4 = raster("2016-04-03_bert_boerma_kale_grond_transparent_reflectance_nir.tif")
+#f_mask = raster("2016-04-03_bert_boerma_kale_grond_index_cumulative.tif")
+
+#file1 = crop(file1, extent(ext))
+#file2 = crop(file2, extent(ext))
+#file3 = crop(file3, extent(ext))
+#file4 = crop(file4, extent(ext))
+
+#in_stack = stack(file1,file2,file3,file4)
+
+#rm(file1,file2,file3,file4,ext)
+
+
+CalculateIndex = function(in_stack,field_mask = NA, agg_factor = 10, ...)
+#{    
+#  if (missing(field_mask))
+#   {
+#       warning("Mask is essential for better results!")
+#  }
+#   else 
+#   {
+#       in_stack = mask(in_stack, field_mask)
+#   }
+   
+#   in_stack =  aggregate(in_stack, fact= agg_factor)
+#   in_data = getValues(in_stack)
+
+#   # scale=T save scaling applied to each variable, Center = T, save means that were subtracted, retx=F don't save PCA scores
+#   data.pca = princomp(na.omit(in_data), scale = F, center = F, retx = F)
+#   new_data = predict(data.pca, in_data)
+#   new_raster = raster(in_stack[[1]])
+#   new_raster = setValues(in_stack[[1]], new_data[,1])
+   
+
+#   return(new_raster)
+#}
+
+#CalculateIndex(in_stack, f_mask, 5)
