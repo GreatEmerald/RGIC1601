dcad1e24db59c2ca92bd666329c3ce6736524254
GRathee <grathee@geetika.waterinsight.nl>
2016-06-17 19:26:30 +0200

    sanity checks!

--- backend/src/modules/HomogeniseRaster.r
+++ backend/src/modules/HomogeniseRaster.r
@@ -17,43 +17,56 @@
 #### Required libraries ####
 library(raster)
 
-## Function for making homogeneous zones out of heterogenous MZ raster.
-#
+#### Homogenizes the clustered zones ####
+
 # Arguments:
-#   rast_in:
+#   zoneRaster:
 #       character string: Management Zone Raster. Must be a format handled by GDAL.
 #       character vector: RasterLayer to use as input, single-band.
 #           Must be a format handled by GDAL.
+#   window_type:
+#       window type is used as the moving window in focal function 
 #   
 # Maintains:
 #   Environment
 #   Input files
 #
 # On violation:
-#   Stops script and gives a message
+#   stop()
 #
 # Returns:
-#   A list of 4, with the Homogenized raster in [[2]].
-#   The amount of Management Zones equals the amount of elements.
+#   a homogenised raster with integer value as zone number value
 
-# INTO THE FUNCTION
-HomogeniseRaster = function(x, window_type, min_ar, ...)
+
+HomogeniseRaster = function(zoneRaster, window_type, min_ar, ...)
 {
-  if (nbands(x) > 1)
+  if (nbands(zoneRaster) > 1)
   {
-    stop(paste("Input", (data.class(x)), "is not single-banded."))
+      stop("Input needs to be a single-band raster! /n Alternatively, choose a band of raster as an input object")
   }
-  if (window_type == "circle")
+  if (window_type != "circle")
   {
-      radius = sqrt(min_ar * 10000 / 3.14)
-      cf = focalWeight(x, radius/2, type = window_type)
-      new_raster = focal(x, w = cf, fun = modal, na.rm = T)
+      stop("Function can only handle circular window type!")
+  }
+  else
+  {
+      radius = sqrt(min_ar * 10000 / 3.14) / 2
+      cf = focalWeight(zoneRaster, radius, type = window_type)
+      new_raster = focal(zoneRaster, w = cf, fun = modal, na.rm = T)
+      # the focal function uses modal function, which substitues the value of central cell 
+      # with the value, whcih is in majority in the moving window
+      
       zones = c(unique(new_raster))
       
       for(i in 1:length(zones))
       {
           new_raster[new_raster == zones[i]] = i-1
       }
+      
+      metadata(new_raster) = append(metadata(zoneRaster), list(filterType = "modal", 
+				      windowType = window_type))
+      # size of each zone
+      
       
       if (missing(...))
       {
@@ -64,8 +77,6 @@ HomogeniseRaster = function(x, window_type, min_ar, ...)
           new_raster = writeRaster(new_raster, dataType = "INT1S", overwrite = T, ...)
           return(new_raster)
       }
-      
   }
-  
 }
 
