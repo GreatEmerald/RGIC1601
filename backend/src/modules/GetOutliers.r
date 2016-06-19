449d629433690ed111765e2134a5e95f21d38693
wschuc002 <william.schuch@wur.nl>
2016-06-20 00:16:16 +0200

    ...

--- backend/src/modules/GetOutliers.r
+++ backend/src/modules/GetOutliers.r
@@ -15,6 +15,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #### Required libraries ####
+#install.packages(c("nominatim", "raster", "rgdal", "sp", "spatstat", "maptools", "rgeos"))
+#install.packages("igraph")
 library(raster)
 library(rgdal)
 library(sp)
@@ -45,54 +47,130 @@ library(igraph)
 #   with the outliers.
 
 
-# INTO THE FUNCTION
 GetOutliers = function(rast_in, Q)
 {
   if (nbands(rast_in) > 1)
   {
     stop(paste("Input", (data.class(rast_in)), "is not single-banded."))
   }
+  if (class(Q) != "numeric")
+  {
+    stop("Quantile value (Q) has to be a Numeric variable")
+  }
+  if (Q>1 | Q<0)
+  {
+    stop("Quantile value (Q) must be between 0 and 1.")
+  }
   UV = unique(rast_in)
   
   if (length(UV) > 20) # used for 'Singe-band image'
   {
     quantiles = quantile(rast_in, c(Q,1-Q))
-    Vec = rasterToPoints(rast_in, fun=function(x){x>quantiles[[2]] | x<quantiles[[1]]}, spatial=TRUE)
-    #spplot(rast_in, sp.layout=list(Vec))
+    #Vec = rasterToPoints(rast_in, fun=function(x){x>quantiles[[2]] | x<quantiles[[1]]}, spatial=TRUE)
+    Vec_upper = rasterToPoints(rast_in, fun=function(x){x>quantiles[[2]]}, spatial=TRUE)
+    Vec_lower = rasterToPoints(rast_in, fun=function(x){x<quantiles[[1]]}, spatial=TRUE)
+    
+    #sSp = as(Vec, "ppp")  # convert points to pp class
+    sSp_upper = as(Vec_upper, "ppp")  # convert points to pp class
+    sSp_lower = as(Vec_lower, "ppp")  # convert points to pp class
+    
+    #Dens = density(sSp, adjust = 0.1)  # create density object
+    Dens_upper = density(sSp_upper, adjust = 0.1)  # create density object
+    Dens_lower = density(sSp_lower, adjust = 0.1)  # create density object
+    
+    #Dsg = as(Dens, "SpatialGridDataFrame")  # convert to spatial grid class
+    Dsg_upper = as(Dens_upper, "SpatialGridDataFrame")  # convert to spatial grid class
+    Dsg_lower = as(Dens_lower, "SpatialGridDataFrame")  # convert to spatial grid class
+    
+    #Dim = as.image.SpatialGridDataFrame(Dsg)  # convert again to an image
+    Dim_upper = as.image.SpatialGridDataFrame(Dsg_upper)  # convert again to an image
+    Dim_lower = as.image.SpatialGridDataFrame(Dsg_lower)  # convert again to an image
+    
+    #Dcl = contourLines(Dim, nlevels = 8)  # create contour object - change 8 for more/fewer levels
+    Dcl_upper = contourLines(Dim_upper, nlevels = 8)  # create contour object - change 8 for more/fewer levels
+    Dcl_lower = contourLines(Dim_lower, nlevels = 8)  # create contour object - change 8 for more/fewer levels
+    
+    #SLDF = ContourLines2SLDF(Dcl)  # convert to SpatialLinesDataFrame
+    SLDF_upper = ContourLines2SLDF(Dcl_upper)  # convert to SpatialLinesDataFrame
+    SLDF_lower = ContourLines2SLDF(Dcl_lower)  # convert to SpatialLinesDataFrame
+    
+    #Polyclust = gPolygonize(SLDF[5, ])
+    Polyclust_upper = gPolygonize(SLDF_upper[5, ])
+    Polyclust_lower = gPolygonize(SLDF_lower[5, ])
+    
+    #gas = gArea(Polyclust, byid = T)/10000
+    gas_upper = gArea(Polyclust_upper, byid = T)/10000
+    gas_lower = gArea(Polyclust_lower, byid = T)/10000
+    
+    PROJ = rast_in@crs@projargs
+    
+    #Polyclust = SpatialPolygonsDataFrame(Polyclust, data = data.frame(gas), match.ID = F)
+    Polyclust_upper = SpatialPolygonsDataFrame(Polyclust_upper, data = data.frame(gas_upper), match.ID = F)
+    Polyclust_lower = SpatialPolygonsDataFrame(Polyclust_lower, data = data.frame(gas_lower), match.ID = F)
+    
+    print("After Polyclust_lower")
+    
+    #centroids = getSpPPolygonsLabptSlots(Polyclust)
+    centroids_upper = getSpPPolygonsLabptSlots(Polyclust_upper)
+    centroids_lower = getSpPPolygonsLabptSlots(Polyclust_lower)
+    
+    print("After centroids_lower")
+    
+    #centroidsDF = as.data.frame(centroids)
+    centroidsDF_upper = as.data.frame(centroids_upper)
+    centroidsDF_lower = as.data.frame(centroids_lower)
+    
+    print("After centroidsDF_lower")
+    
+    #centroidsSPDF = SpatialPointsDataFrame(centroidsDF, data = data.frame(gas))
+    centroidsSPDF_upper = SpatialPointsDataFrame(centroidsDF_upper, data = data.frame(gas_upper))
+    centroidsSPDF_lower = SpatialPointsDataFrame(centroidsDF_lower, data = data.frame(gas_lower))
+    
+    print("After centroidsSPDF_lower")
+    
+    # culculate mean
+    WSmean = cellStats(rast_in,mean)
+    
+    tering = SpatialPointsDataFrame(list(centroidsSPDF_upper,centroidsSPDF_lower), data = data.frame(gas_lower))
+    
+    print("tering")
   
-    sSp = as(Vec, "ppp")  # convert points to pp class
-    Dens = density(sSp, adjust = 0.1)  # create density object
-    Dsg = as(Dens, "SpatialGridDataFrame")  # convert to spatial grid class
-    Dim = as.image.SpatialGridDataFrame(Dsg)  # convert again to an image
-    Dcl = contourLines(Dim, nlevels = 8)  # create contour object - change 8 for more/fewer levels
-    SLDF = ContourLines2SLDF(Dcl)  # convert to SpatialLinesDataFrame
-    Polyclust = gPolygonize(SLDF[5, ])
-    gas = gArea(Polyclust, byid = T)/10000
-    Polyclust = SpatialPolygonsDataFrame(Polyclust, data = data.frame(gas), match.ID = F)
-    centroids = getSpPPolygonsLabptSlots(Polyclust)
-    centroidsDF = as.data.frame(centroids)
-    centroidsSPDF = SpatialPointsDataFrame(centroidsDF, data = data.frame(gas))
-  
-    return(list(rast_in, quantiles, Vec, Polyclust, centroidsSPDF, Dsg))
+    return(list(rast_in, #input raster
+                 Vec_upper,Vec_lower, #Up&Lo vectors
+                 centroidsSPDF_upper,centroidsSPDF_lower, #Up&Lo centroids
+                  Dcl_upper, Dcl_lower, #Up&Lo contour lines
+                 Polyclust_upper, Polyclust_lower
+                 ))
+    
+    #return(tering)
   }
-  # if (length(UV) <= 20) # used for 'Heterogeneous MZ raster'
-  # {
-  #   Vec_MZ2 = RasterToVector(rast_in[[2]])
-  #   gas = gArea(Vec_MZ2, byid = T)/10000
-  #   Polyclust = SpatialPolygonsDataFrame(Vec_MZ2, data = data.frame(gas), match.ID = F)
-  #   
-  #   #centroids = getSpPPolygonsLabptSlots(Polyclust)
-  #   #centroidsDF = as.data.frame(centroids)
-  #   #centroidsSPDF = SpatialPointsDataFrame(centroidsDF, data = data.frame(gas))
-  #   
-  #   #Dcl = contourLines(rast_in, nlevels = 1)  # create contour object - change 8 for more/fewer levels
-  #   
-  #   return(list(rast_in, Vec_MZ2, gas, Polyclust, centroidsSPDF))
-  # }
 }
-# GetOutl = GetOutliers(WS,0.0005) # used for 'Singe-band image'
-# GetOutl_MZ = GetOutliers(Zones_PC5[[2]],0.005) # used for 'Homogeneous MZ raster'
+# GetOutl = GetOutliers(WS,0.003) # used for 'Singe-band image'
+# plot(GetOutl)
+# 
+# rv_pnt_up = list("sp.points", GetOutl[[2]], col = "red")
+# rv_pnt_lo = list("sp.points", GetOutl[[3]], col = "green")
+# rv_cent_up = list("sp.points", GetOutl[[4]], col = "yellow")
+# rv_cent_lo = list("sp.points", GetOutl[[5]], col = "white")
+# rv_pol_up = list("sp.polygons", GetOutl[[8]], fill = "white")
+# rv_pol_lo = list("sp.polygons", GetOutl[[9]], fill = "white")
+# 
+# SPplotWS = spplot(GetOutl[[1]], scales = list(draw = TRUE),
+#                   xlab = "X", ylab = "Y",
+#                   ol.regions = rainbow(99, start=.1),
+#                   sp.layout = list(rv_pnt_up,rv_pnt_lo))
+# SPplotWS
+# 
+# spplot(GetOutl[[1]])
+# spplot(GetOutl[[3]], add=TRUE)
 # spplot(GetOutl[[4]])
+
+
+
+
+
+
+
 # 
 # spplot(Zones_PC5[[1]])
 # unique(Zones_PC5)
